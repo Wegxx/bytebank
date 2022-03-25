415b45a0422a58c5f8f032451c9c289862e3d737
giovannanascimento-hotmart <91634590+giovannanascimento-hotmart@users.noreply.github.com>
2022-03-25 18:03:27 -0300

    High Order Functions 2

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,27 +1,43 @@
+import br.com.alura.bytebank.modelo.Autenticavel
 import br.com.alura.bytebank.modelo.Endereco
+import br.com.alura.bytebank.modelo.SistemaInterno
+import java.util.*
 
 fun main() {
-    val testeFuncao: () -> Unit
-   Endereco().let {
-       it
-   }
-    "".let(:: testeRecebeString)
 
-    1.let {
-        it
+    //val endereco =  Endereco(logradouro = "rua vergueiro", numero = 3185)
+    //val enderecoEmMaiusculo = "${endereco.logradouro}, ${endereco.numero}".uppercase(Locale.getDefault())
+    //println(enderecoEmMaiusculo)
+
+   Endereco(logradouro = "rua vergueiro", numero = 3185).let { endereco ->
+        "${endereco.logradouro}, ${endereco.numero}".uppercase(Locale.getDefault())
+   }.let (::println)
+
+    val enderecosComComplemento = listOf(Endereco(complemento = "casa"),
+        Endereco(complemento = "apartamento"),
+        Endereco()).filter (predicate = { endereco -> endereco.complemento.isNotEmpty() }).let (block = (::println))
+
+    soma (1, 5, resultado = (:: println))
+
+    val autenticavel = object : Autenticavel {
+        val senha = 1234
+        override fun autenticacao(senha: Int) = this.senha == senha
+
     }
 
-    teste (1, {})
+    SistemaInterno().entrar(autenticavel, 1234, autenticado = {
+        println("Realiza pagamento")
+    })
 
 }
 
-    //Higher order function
-    fun teste( teste: Int, bloco: () -> Unit,){
+fun soma(a: Int, b: Int, resultado: (Int) -> Unit){
+    println("antes da soma")
+    resultado(a + b)
+    println("depois da soma")
+}
 
-    }
-    fun testeRecebeString(valor: String){
 
-    }
 
 
 
--- src/main/kotlin/br/com/alura/bytebank/modelo/SistemaInterno.kt
+++ src/main/kotlin/br/com/alura/bytebank/modelo/SistemaInterno.kt
@@ -2,9 +2,10 @@ package br.com.alura.bytebank.modelo
 
 class SistemaInterno {
 
-    fun entrar(admin: Autenticavel, senha: Int){
+    fun entrar(admin: Autenticavel, senha: Int, autenticado: () -> Unit = {}){
         if(admin.autenticacao(senha)){
             println("Bem Vindo ao ByteBank")
+            autenticado()
         } else {println("Falha na Autenticação")}
     }
 

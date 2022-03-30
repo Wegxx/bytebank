22a16fba2412e9a42c1c1f45d52998b28c6d0890
giovannanascimento-hotmart <91634590+giovannanascimento-hotmart@users.noreply.github.com>
2022-03-30 14:51:01 -0300

    Receiver

--- src/main/kotlin/br/com/alura/bytebank/modelo/SistemaInterno.kt
+++ src/main/kotlin/br/com/alura/bytebank/modelo/SistemaInterno.kt
@@ -9,4 +9,15 @@ class SistemaInterno {
         } else {println("Falha na Autenticação")}
     }
 
+    fun entrarReceiver(admin: Autenticavel, senha: Int, autenticado: SistemaInterno.() -> Unit = {}){
+        if(admin.autenticacao(senha)){
+            println("Bem Vindo ao ByteBank")
+            autenticado(this)
+        } else {println("Falha na Autenticação")}
+    }
+
+    fun pagamento(){
+        println("realziando pagamento")
+    }
+
 }
--- src/main/kotlin/br/com/alura/bytebank/teste/testaHOF.kt
+++ src/main/kotlin/br/com/alura/bytebank/teste/testaHOF.kt
@@ -4,13 +4,26 @@ import br.com.alura.bytebank.modelo.Autenticavel
 import br.com.alura.bytebank.modelo.SistemaInterno
 
 fun testaHOF() {
-    soma(1, 5, resultado = (::println))
+    soma(1, 5, resultado = {valor -> //Soma por high order function
+        println(valor)}
+    )
+    somaReceiver(1, 5, resultado = { //Soma por Receiver
+        println(this)
+    })
 
     val autenticavel = object : Autenticavel {
         val senha = 1234
         override fun autenticacao(senha: Int) = this.senha == senha
 
     }
+
+    val sistema = SistemaInterno()
+    sistema.entrar(autenticavel, 1234, autenticado = {
+        println("realizar pagamento")
+    })
+    sistema.entrarReceiver(autenticavel, 1234, autenticado = {
+        pagamento()
+    })
 
     SistemaInterno().entrar(autenticavel, 1234, autenticado = {
         println("Realiza pagamento")
@@ -23,8 +36,9 @@ fun soma(a: Int, b: Int, resultado: (Int) -> Unit){
     println("depois da soma")
 }
 
-fun somaReceiver(a: Int, b: Int, resultado: (Int) -> Unit){
+fun somaReceiver(a: Int, b: Int, resultado: Int.() -> Unit){
     print("antes da soma")
-    resultado(a + b)
-    println("depois do resultado")
+    val total = a + b
+    total.resultado()
+    println("depois da soma ")
 }
